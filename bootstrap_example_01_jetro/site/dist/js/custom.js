ee625621ee0974b592a36863aeb285bb50e09eb2
maximebisantz <maxime.bisantz@gmail.com>
2016-11-13 19:39:49 +0100

    13th nov 2016 - progress on home page

--- bootstrap_example_01_jetro/site/dist/js/custom.js
+++ bootstrap_example_01_jetro/site/dist/js/custom.js
@@ -18,20 +18,34 @@ $(document).ready(function(){
 	 * Animate the caption on carousel.slide event.
 	 */
 	$('#home-carousel').on('slid.bs.carousel', function(e){
-		/**
-		 * Sorted out with CSS selector only.
-		 */
-//		console.log(this);
-//		$this = this;
-//		
-//		$caption = $('.active .carousel-caption').first();
-
-//		$caption.css('display: none');
-//		void($caption.offsetWidth);
-//		$caption.css('display: block');
 		
+		/**
+		 * Dirty trick to put the class "active" on the related 
+		 * carousel indicator. (because it's different from 
+		 * bootstrap default indicators, it didn't follow the 
+		 * slides as they slid)
+		 */
+		
+		// select custom indicators
+		$indicators = $('.carousel-indicators-item');
+		
+		// Find active indicator and remove the active class
+		$active = $indicators.filter('.active');
+		$active.removeClass('active');
+		
+		// From the relatedTarget event object. Custom ID provided
+		$currentSlide = e.relatedTarget.id.replace("carousel-item-", "");
+		
+		// Select the indicator of the current slide and attach the active class
+		$next = $indicators[$currentSlide - 1];
+		$($next).addClass('active');
 		
 	})
+	
+	
+	// Display a modal for styling
+	//$('#article-1-more').modal('show');
+	
 	
 	
 	
